17a7ef20adde8daabe23e1b09ae3cbae0d3878ef
Misha Lunov <lunc8h11n@gmail.com>
2019-10-21 22:59:50 +0300

    Delete 13.py

--- Week 1: Integers, I-O, simple string operations/13.py
+++
@@ -1,5 +0,0 @@
-number = int(input())
-print('The next number for the number', number, 'is', number+1, end='')
-print('.')
-print('The previous number for the number', number, 'is', number-1, end='')
-print('.')
